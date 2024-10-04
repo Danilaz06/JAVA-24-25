24aab0616f11cb6e65888fb3f3e504f9998ac519
Danilaz06 <daniellazar1614@gmail.com>
2024-10-04 12:28:36 +0200

    04/10/2024
    primer java

---
+++ Simpson/src/Simpson.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Simpson {
+    //visible a nivel de la clase simpson
+    static char[][] tablero = new char[10][10];
+    //Software de gestión de venta online
+    public static void rellenarTablero(char pers){
+        for (int i = 0; i < 10 ; i++) {
+            for (int j = 0; j < 10 ; j++) {
+                tablero[i][j] = 'L'
+            }
+        }
+        tablero[0][2] 
+    }
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        int contador =0;
+        char personaje ='#';
+        rellenarTablero(personaje);
+
+        rellenarTablero(contador);
+
+        //for ()
+        }
+    }
