3c20a0e6433e095b45859be6755b8fd7a6bf2689
Danilaz06 <daniellazar1614@gmail.com>
2024-10-04 12:57:26 +0200

    12:57 04/10/24

--- Simpson/src/Simpson.java
+++ Simpson/src/Simpson.java
@@ -7,18 +7,25 @@ public class Simpson {
     public static void rellenarTablero(char pers){
         for (int i = 0; i < 10 ; i++) {
             for (int j = 0; j < 10 ; j++) {
-                tablero[i][j] = 'L'
+                tablero[i][j] = pers;
             }
         }
-        tablero[0][2] 
     }
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        int contador =0;
-        char personaje ='#';
-        rellenarTablero(personaje);
+    public static void escribirtablero() {
+        for (int i = 0; i <10; i++) {
+            System.out.println();
+            for (int j = 0; j <10 ; j++) {
+                System.out.println(tablero[i][j]);
+            }
 
-        rellenarTablero(contador);
+    public static void main(String[] args) {
+              char personaje= '#';
+              rellenarTablero(personaje);
+              escribirtablero();
+            }
+
+        }
+
 
         //for ()
         }
